2cd9debc879d2cfbcf2f4e55fc6c7e518947603c
kaktusss123 <PaRoLoK123>
2020-04-06 13:26:35 +0300

    latest updates

--- app_aio.py
+++ app_aio.py
@@ -4,6 +4,7 @@ from aiohttp import ClientSession
 from flask import Flask, request
 from hashlib import sha256
 from json import dumps
+from datetime import date
 
 app = Flask(__name__)
 segments = {
@@ -15,15 +16,20 @@ async def get(lst: list) -> str:
     wrapper = "'"  # обертка для f-строки, а то питон ругается
     seg = segments[lst[0]['segment']]
     keys = ('source', 'link', 'price', 'area')
-    hash_to_id = {sha256("__".join((str(el[key]) for key in keys)).encode('utf-8')).hexdigest(): el['id'] for el in lst}
+    hash_to_id = {sha256("__".join((str(el[key]) for key in keys)).encode(
+        'utf-8')).hexdigest(): el['id'] for el in lst}
     async with ClientSession() as s:
-        client = ChClient(s, url='http://10.199.13.111:8123', database='offers_dims')
+        client = ChClient(s, url='http://10.199.13.111:8123',
+                          database='offers_dims')
         if not await client.is_alive():
             return 'DB connection error'
-        q = f'SELECT hash FROM {seg} WHERE hash IN ({", ".join(map(lambda x: f"{wrapper}{x}{wrapper}", hash_to_id.keys()))})'
-        fetch = await client.fetch(q)
-    selected = list(map(lambda x: x[0], fetch))
-    res = [{'id': id, 'exists': h in selected} for h, id in hash_to_id.items()]
+        q = f'SELECT hash, diap, storage, operation FROM {seg} WHERE hash IN ({", ".join(map(lambda x: f"{wrapper}{x}{wrapper}", hash_to_id.keys()))})'
+        data = await client.fetch(q)
+    selected = list(map(lambda x: x[0], data))
+    res = [{'id': id, 'exists': False, 'diap': None, 'storage': None, 'operation': None}
+           for h, id in hash_to_id.items() if h not in selected]
+    res += [{'id': hash_to_id[h], 'exists': True, 'diap': d,
+             'storage': s, 'operation': o} for h, d, s, o in data]
     return dumps(res)
 
 
@@ -36,14 +42,14 @@ async def put(lst: list) -> str:
         for el in lst:
             try:
                 await client.execute(f'INSERT INTO {segments[el["segment"]]} \
-                (source, link, price, area, hash) \
+                (source, link, price, area, hash, date, diap, storage, operation) \
                 VALUES (\'{el["source"]}\', \'{el["link"]}\', {el["price"]}, {el["area"]}, \
-                \'{sha256("__".join((str(el[key]) for key in keys)).encode("utf-8")).hexdigest()}\')')
+                \'{sha256("__".join((str(el[key]) for key in keys)).encode("utf-8")).hexdigest()}\, \'{date.today()}\', \'{el.get("diap")}\', \'{el.get("storage")}\', \'{el.get("operation")}\')')
             except:
                 pass
-    return 'Success'
-        
-        
+    return {'status': 'Success'}
+
+
 @app.route('/put', methods=['POST'])
 def put_api():
     loop = asyncio.new_event_loop()
@@ -51,12 +57,14 @@ def put_api():
     loop.close()
     return res
 
+
 @app.route('/get', methods=['POST'])
 def get_api():
     loop = asyncio.new_event_loop()
     res = loop.run_until_complete(get(request.json))
     loop.close()
     return res
+
 
 if __name__ == "__main__":
     app.run('10.199.13.111', 9515)
